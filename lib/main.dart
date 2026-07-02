91bf9740e5a70016aa3478ee4b9c321a9547c13b
Sahil Kun <kunsahil42@gmail.com>
2026-07-02 15:48:01 +0530

    Sprint 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
 
 void main() {
-  runApp(const AniSpinApp());
+  runApp(
+    const ProviderScope(
+      child: AniSpinApp(),
+    ),
+  );
 }
